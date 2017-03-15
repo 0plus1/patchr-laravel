fe1b4f70ed37338eda2943ac851a669cf4b6ad23
Davide <davide.0plus1@gmail.com>
2017-03-15 18:11:19 +1100

    ServiceProvider

---
+++ src/ServiceProvider.php
@@ -0,0 +1,53 @@
+<?php
+namespace Zeroplusone\Patchr\Laravel;
+
+use Illuminate\Support\ServiceProvider as IlluminateServiceProvider;
+use Zeroplusone\Patchr\Laravel\Console\Commands\PatchrCLI;
+
+/**
+ * Class ServiceProvider
+ * @package Zeroplusone\Patchr\Laravel
+ */
+class ServiceProvider extends IlluminateServiceProvider {
+
+    /**
+     * Indicates if loading of the provider is deferred.
+     *
+     * @var bool
+     */
+    protected $defer = false;
+
+    /**
+     * Register the service provider.
+     *
+     * @throws \Exception
+     * @return void
+     */
+    public function register()
+    {
+        $configPath = __DIR__ . '/../config/patchr.php';
+        $this->mergeConfigFrom($configPath, 'patchr');
+    }
+
+    /**
+     *
+     */
+    public function boot()
+    {
+        $configPath = __DIR__ . '/../config/patchr.php';
+        $this->publishes([$configPath => config_path('patchr.php')], 'config');
+        $this->cli();
+    }
+
+    /**
+     * Provides the CLI commands to Laravel
+     */
+    private function cli()
+    {
+        if ($this->app->runningInConsole()) {
+            $this->commands([
+                PatchrCLI::class,
+            ]);
+        }
+    }
+}
