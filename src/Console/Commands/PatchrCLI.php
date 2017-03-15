4c52bc13244e63816b560540a54fefdd87392842
Davide <davide.0plus1@gmail.com>
2017-03-15 18:11:09 +1100

    Exposes Patchr CLI commands to laravel command

---
+++ src/Console/Commands/PatchrCLI.php
@@ -0,0 +1,69 @@
+<?php namespace Zeroplusone\Patchr\Laravel\Console\Commands;
+
+use Illuminate\Console\Command;
+use Zeroplusone\Patchr\Patchr;
+
+/**
+ * Class PatchrCLI
+ * @package Zeroplusone\Patchr\Laravel\Console\Commands
+ * TODO: Refactor into patchr:do {extra} format
+ */
+class PatchrCLI extends Command {
+
+    /**
+     * The console command name.
+     *
+     * @var string
+     */
+    protected $signature = 'patchr {do} {extra?}';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Run a patchr command';
+
+    /**
+     * Execute the console command.
+     *
+     * @return mixed
+     */
+    public function handle()
+    {
+        $patchr = new Patchr( config('patchr') );
+
+        switch( $this->argument('do') )
+        {
+            case 'applyDatabasePatches':
+                $output = $patchr->applyDatabasePatches();
+                break;
+            case 'reservePatches':
+                if( !$this->argument('extra') )
+                {
+                    $this->error('Missing Number of Patches to reserve');
+                    return;
+                }
+                $output = $patchr->reservePatches( $this->argument('extra') );
+                break;
+            case 'addNewPatch':
+                if( !$this->argument('extra') )
+                {
+                    $this->error('Missing SQL Code');
+                    return;
+                }
+                $output = $patchr->addNewPatch( $this->argument('extra') );
+                break;
+            case 'getUnappliedPatches':
+                $output = $patchr->getUnappliedPatches();
+                break;
+            case 'getLatestAppliedPatch':
+                $output = $patchr->getLatestAppliedPatch();
+                break;
+            default:
+                $this->error('Command not found: ['.$this->argument('do').']');
+                break;
+        }
+    }
+
+}
